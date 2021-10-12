ca7b3ec859be30fdcb3346d749733efa306be629
Sekar <fs@sekars-laptop.local>
2021-10-12 14:30:16 +0700

    Project file

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    // Menambah jumlah cube yang dimiliki
+    public int score;
+    public Text scoreText;
+
+    public void IncreaseScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+}
