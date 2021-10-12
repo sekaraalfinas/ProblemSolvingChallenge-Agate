7da4e55137bfdb30bc0dca600e59bf52978e3723
Sekar <fs@sekars-laptop.local>
2021-10-12 12:49:30 +0700

    Project file

---
+++ Assets/Scripts/BallMovement2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement2 : MonoBehaviour
+{
+
+    public float movementSpeed;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+}
