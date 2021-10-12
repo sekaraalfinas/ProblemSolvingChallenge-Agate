f3952c6a1816bcd82563d8ec70da1c4e7dc9e699
Sekar <fs@sekars-laptop.local>
2021-10-12 18:54:16 +0700

    Project File

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    public void ProblemOne()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 1);
+    }
+
+    public void ProblemTwo()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 2);
+    }
+
+    public void ProblemThree()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 3);
+    }
+
+    public void ProblemFour()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 4);
+    }
+
+    public void ProblemFive()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 5);
+    }
+
+    public void ProblemSix()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 6);
+    }
+
+    public void ProblemSeven()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 7);
+    }
+
+    public void ProblemEight()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 8);
+    }
+
+    public void ProblemNine()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 9);
+    }
+}
+
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    // Menambah jumlah cube yang dimiliki
+    public int score;
+    public Text scoreText;
+
+    // Game Over Panel
+    public GameObject gameOverPanel;
+
+    private void Awake()
+    {
+        gameOverPanel.SetActive(false);
+    }
+
+    public void IncreaseScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+
+    public void OnEnemyHit()
+    {
+        Time.timeScale = 0;
+        gameOverPanel.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        score = 0;
+        scoreText.text = "0";
+
+        Destroy(GameObject.FindGameObjectWithTag("cube"));
+        GameObject.FindGameObjectWithTag("Player").transform.position = new Vector3(0, 0, 0);
+        GameObject.FindGameObjectWithTag("Enemy").transform.position = new Vector3(-3, 0, 0);
+
+        gameOverPanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
